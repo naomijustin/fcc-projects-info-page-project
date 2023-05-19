4e9c703a7cc6ff53ae85bcd61aab3fe6a694111f
naomijustin <naomi.z.justin@gmail.com>
2023-05-19 10:38:27 +1000

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import logo from "./logo.svg"
+import style from "./index.css"
+/**
+Challenge: 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+- Add an `ul` inside the Header's `nav` and create
+  the following `li`s: "Pricing", "About", & "Contact"
+*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+  return(
+    <header>
+        <nav>
+          <img src={logo} className="imgLogo"></img>
+          <ul className="nav-items">
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      &emsp;&#169;<small>2023 Zippurah development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function Content() {
+  return (
+    <div className="mainContent">
+      <h1>React info</h1>
+      <ol>
+        <li>Learn new technology</li>
+        <li>Upskill</li>
+      </ol>
+    </div>
+  )
+}
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Page />);
